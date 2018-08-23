e86584de351ea27abef86735a626c180a6ff5e29
Robert Chen <robertchen234@gmail.com>
2018-08-23 02:37:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,22 +12,19 @@ const codes = [
 ];
 
 function init() {
-  function init() {
-  // your code here	  var index = 0;
-  document.body.addEventListener('keydown',
-  function onKeyDownHandler(e) {
-    const key = e.key;
-    if (key === codes[index]) {
-      index++;
-        console.log(index);
-        console.log(key);
-      if (index === codes.length) {
-        window.alert("Konami Code Achieved");
-          index = 0;
-      }
-    } else {
-      index = 0;
+document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+  const key = e.key;
+  if (key === codes[index]) {
+    index++;
+    console.log(index);
+    console.log(key);
+    if (index === codes.length) {
+      window.alert("Konami Code Achieved");
+        index = 0;
+    }
+  } else {
+    index = 0;
     }
   });
 }
-}
+
